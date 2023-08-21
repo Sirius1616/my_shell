8eab0f42cf9d648455082f94d053c74d57922efe
Sirius1616 <Siriusa1.615@gmail.com>
2023-08-21 22:14:01 +0000

    shell

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
+ * main - starting point
+ * @ac: count of argument
+ * @av: vector arg
+ * Return: 1 on success, 0 on error
  */
 int main(int ac, char **av)
 {
-	info_t info[] = { INFO_INIT };
+	t_info details[] = { DATA_INIT };
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
@@ -26,19 +25,19 @@ int main(int ac, char **av)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
+				puts_err(av[0]);
+				puts_err(": 0: No access ");
+				puts_err(av[1]);
+				putchar_err('\n');
+				putchar_err(BUFFER_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		details->read_file_desc = fd;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
+	fill_env_list(details);
+	rd_hist(details);
+	hsh(details, av);
 	return (EXIT_SUCCESS);
 }
